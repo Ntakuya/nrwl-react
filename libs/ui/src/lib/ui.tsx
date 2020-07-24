c5773b2688f8ca678070218eca27a361827e0231
NTakuya <nagahama.takuya@outlook.jp>
2020-07-24 22:57:40 +0900

    first commit

---
+++ libs/ui/src/lib/ui.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface UiProps {}
+
+export const Ui = (props: UiProps) => {
+  return (
+    <div>
+      <h1>Welcome to ui!</h1>
+    </div>
+  );
+};
+
+export default Ui;
